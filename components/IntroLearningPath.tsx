02d22c0d35ba08180f854b493755e24979de2046
Violedu <152742289+Violedu@users.noreply.github.com>
2023-12-14 16:51:34 +0100

    Create IntroLearningPath.tsx

---
+++ components/IntroLearningPath.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import styles from "./IntroLearningPath.module.css";
+
+const IntroLearningPath: NextPage = () => {
+  return (
+    <div
+      className={styles.introLearningPath}
+      data-scroll-to="introLearningPath"
+    >
+      <div className={styles.title}>
+        <b className={styles.introTitle}>Taylor your learning path.</b>
+      </div>
+    </div>
+  );
+};
+
+export default IntroLearningPath;
