738d1ba8c28e572135bc0a954d6cac0799cc13b9
grgivo <ivosgeorgiev@yahoo.de>
2023-12-22 20:23:35 +0100

    updated terms and policy

--- components/PrivacyContent.tsx
+++ components/PrivacyContent.tsx
@@ -9,60 +9,110 @@ const PrivacyContent: NextPage = () => {
       </div>
       <div className={styles.content}>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
           <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            We acknowledge and value the privacy considerations of our website
+            users at www.violedu.com, as well as those utilizing the associated
+            services. This privacy statement is presented to clarify the
+            collection of information that occurs when visitors access the site
+            and explain potential uses of such gathered information. By using
+            any services on this site, you agree to the terms of this privacy
+            policy. You shouldn’t use any services if you don’t agree with this
+            privacy policy.
           </div>
         </div>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
+          <div className={styles.title1}>Information We Collect</div>
           <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            We collect various types of information when you submit a lesson
+            request, including your full name, email, age, years of playing the
+            violin, country of residence, preferred teaching method (in person
+            or online), and your chosen learning path (single or multiple
+            lessons). This information is utilized to process your lesson
+            requests, communicate with you about scheduling and updates, and
+            enhance our platform and services. We may utilize your contact
+            details to share updates about our platform and products with you.
+            If you wish to discontinue receiving such communications, you can
+            easily opt out using the options provided below.
           </div>
         </div>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
+          <div className={styles.title1}>Choice/Opt-Out</div>
           <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Our website may offer you the choice to subscribe to communications
+            when we collect information from you. You can always choose to
+            exclude yourself from any email list, thus stopping future
+            communications. To remove your name from our list, you can also send
+            an email to contact@violedu.com. Please attach the unwanted email to
+            your request and explicitly express your desire to be removed from
+            our mailing list.
           </div>
         </div>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
-          <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          <div className={styles.title1}>
+            Information Sharing and Disclosure
           </div>
+          <div className={styles.text}>
+            We do not sell, trade, or otherwise transfer your personally
+            identifiable information to third parties. We reserve the right to
+            disclose user information under specific circumstances. This action
+            may be undertaken when there is a reasonable basis to believe that
+            the disclosure is essential for the identification, contact, or
+            initiation of legal proceedings against an individual who might be
+            causing harm or interference, whether intentional or unintentional,
+            to the site's rights, property, other site users, or any other party
+            at risk of harm due to such activities. Additionally, we may find
+            ourself obligated to reveal personal information in compliance with
+            lawful requests from public authorities. This includes instances
+            where such disclosure is necessary to meet national security or law
+            enforcement requirements.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Security</div>
+          <div className={styles.text}>
+            We are committed to ensure the security of your information. We
+            implement a variety of security measures to maintain the safety of
+            your personal information.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Links to External Sites</div>
+          <div className={styles.text}>
+            We are not responsible for the content or practices of third-party
+            websites linked to our platform. We are also not responsible for any
+            information that visitors might share with such linked websites.
+            Visitors should refer to each website’s respective privacy policy
+            and practices prior to disclosing any information.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Changes to this Privacy Policy</div>
+          <div className={styles.text}>
+            We may update this privacy policy from time to time. Any changes
+            will be posted on this page, and the date of the latest revision
+            will be indicated at the bottom.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Contact Us</div>
+          <div className={styles.text}>
+            If you have any questions or concerns about our privacy policy,
+            please contact us at contact@violedu.com.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Your Acceptance of These Terms</div>
+          <div className={styles.text}>
+            By using this website, you agree to follow the rules and limitations
+            stated in this privacy policy. If you don't agree with these rules,
+            please do not use this site. This privacy policy may be revised from
+            time to time by updating this posting. You are bound by any such
+            revisions and should therefore periodically visit this page to
+            review the current privacy policy to which you are bound.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.text}>Last Updated: January 1, 2024</div>
         </div>
       </div>
     </div>
--- components/TermsContent.tsx
+++ components/TermsContent.tsx
@@ -9,60 +9,150 @@ const TermsContent: NextPage = () => {
       </div>
       <div className={styles.content}>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
+          <div className={styles.title1}>
+            Lesson Duration and Future Acceptance
+          </div>
           <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Each individual lesson has a duration of 45 minutes. Purchasing
+            current lessons doesn’t guarantee future enrollment. Decisions about
+            subsequent lessons will be made separately.
           </div>
         </div>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
+          <div className={styles.title1}>Student Responsibilities</div>
           <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Students are expected to attend punctually and adequately prepared.
+            This entails instrumental practice, ensuring a stable internet
+            connection, functional audio/video equipment, a charged device, and
+            having all necessary sheet music ready (for an online lesson). If
+            rescheduling a lesson is needed, the student should provide a
+            24-hour notice. Recording lessons without consent or sharing
+            instructional material is prohibited. Being late will not extend the
+            standard 45-minute lesson duration.
           </div>
         </div>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
+          <div className={styles.title1}>
+            Live Lessons at Student's Residence
+          </div>
           <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Live lessons are conducted at the student's residence within the
+            Stuttgart town area. The student must provide a suitable environment
+            conducive to learning, including a quiet space and proper lighting.
+            The student is responsible for ensuring the safety and security of
+            the location where the lesson will take place. In cases where the
+            residence environment is deemed unsuitable for instruction (e.g.,
+            excessive noise, inadequate space), the teacher reserves the right
+            to cancel or reschedule the lesson.
           </div>
         </div>
         <div className={styles.paragraph}>
-          <div className={styles.title1}>Title</div>
+          <div className={styles.title1}>Free Assessment Session</div>
           <div className={styles.text}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            An invitation to an assessment session does not guarantee immediate
+            enrollment into the program. This session provides an opportunity
+            for the teacher to assess the student's proficiency level and ensure
+            compatibility between the teacher's methodology and the individual
+            needs and goals of the student. Its primary aim is to establish
+            mutual understanding, thus optimizing the learning experience. The
+            session will be conducted online and lasts for 30 minutes. Please
+            note, the teacher reserves the right to decline enrollment if these
+            criteria are not met during the session
           </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Level Determination</div>
+          <div className={styles.text}>
+            Admission to the program typically requires a minimum of 10 years of
+            experience playing the instrument, given the program's advanced
+            level. Nevertheless, students with less experience are encouraged to
+            apply and will be assessed during the free session to determine
+            their eligibility for the program.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Payment</div>
+          <div className={styles.text}>
+            Each lesson/learning path is payable via bank account provided via
+            e-mail. Service and transaction fees are the responsibility of the
+            student.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Rescheduling</div>
+          <div className={styles.text}>
+            Any requests for rescheduling must be made with a minimum 24-hour
+            notice. Rescheduled lessons should be within two weeks and subject
+            to the Teacher's availability. Valid reasons for rescheduling
+            include physical injury (supported by evidence), health challenges
+            (with a medical certificate), or mourning. Failing to reschedule
+            within the timeframe of 3 days, without valid reasons, will not be
+            accommodated.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>
+            Lesson Cancellation or Changes by Teacher
+          </div>
+          <div className={styles.text}>
+            The teacher may need to cancel or reschedule a live or online lesson
+            due to unforeseen circumstances or emergencies. In such cases,
+            reasonable efforts will be made to reschedule the lesson at a
+            mutually convenient time.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Ending the Agreement</div>
+          <div className={styles.text}>
+            The teacher reserves the right to terminate the agreement for
+            several reasons including failure to follow the lessons guidelines,
+            mistreatment, payment issues, behavioral concerns, or other valid
+            grounds. In the event of termination for these reasons, the Student
+            remains responsible for payment. No further monthly payments are
+            required post-termination; however, any previously made payments
+            will be retained as compensation for any inconvenience caused.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Money Back Garantee</div>
+          <div className={styles.text}>
+            All learning paths include a 14-day money back garantee. Refund
+            request should be made within 14 days from the date of enrollment or
+            purchase. The guarantee might apply to specific circumstances such
+            as dissatisfaction with the teaching methodology, course content, or
+            instructor interaction. The refund policy does not cover cases where
+            a student has violated the terms of service or engaged in
+            misconduct.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Changes to this Terms Policy</div>
+          <div className={styles.text}>
+            We may update this terms policy from time to time. Any changes will
+            be posted on this page, and the date of the latest revision will be
+            indicated at the bottom.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Contact Us</div>
+          <div className={styles.text}>
+            If you have any questions or concerns about our terms policy, please
+            contact us at contact@violedu.com.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.title1}>Your Acceptance of These Terms</div>
+          <div className={styles.text}>
+            By using this website, you agree to follow the rules and limitations
+            stated in this terms policy. If you don't agree with these rules,
+            please do not use this site. This terms policy may be revised from
+            time to time by updating this posting. You are bound by any such
+            revisions and should therefore periodically visit this page to
+            review the current terms policy to which you are bound.
+          </div>
+        </div>
+        <div className={styles.paragraph}>
+          <div className={styles.text}>Last Updated: January 1, 2024</div>
         </div>
       </div>
     </div>
