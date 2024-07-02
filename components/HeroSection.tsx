e8e5d9cecc5224a00684b42a950ee4570ab9de2c
violedu <violeduinternal@gmail.com>
2024-07-02 16:18:29 +0200

    added animation for hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+import { animate } from "motion"
 import type { NextPage } from "next";
 import { useCallback } from "react";
 import styles from "./HeroSection.module.css";
@@ -10,10 +12,23 @@ const HeroSection: NextPage = () => {
     }
   }, []);
 
+  const actionCallRef = useRef<HTMLDivElement>(null);
+  const imgRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (actionCallRef.current) {
+      animate(actionCallRef.current, { opacity: [0, 1], y: [24, 0] }, { duration: 0.8 });
+    }
+
+    if (imgRef.current) {
+      animate(imgRef.current, { opacity: [0, 1], y: [10, 0] }, { duration: 1 });
+    }
+  }, []);
+
   return (
     <div className={styles.heroSection}>
       <div className={styles.heroSection1}>
-        <div className={styles.actionCall}>
+        <div className={styles.actionCall} ref={actionCallRef}>
           <div className={styles.text}>
             <div className={styles.title}>
               <p className={styles.stepUp}>{`Step Up `}</p>
@@ -31,8 +46,10 @@ const HeroSection: NextPage = () => {
             <div className={styles.requestALesson}>Request a Lesson</div>
           </button>
         </div>
-        <img className={styles.imageIcon} alt="" src="/image-1@2x.png" />
-        <img className={styles.imageIcon1} alt="" src="/image-11@2x.png" />
+        <div ref={imgRef}>
+          <img className={styles.imageIcon} alt="" src="/image-1@2x.png" />
+          <img className={styles.imageIcon1} alt="" src="/image-11@2x.png" />
+        </div>
       </div>
     </div>
   );
