2d30b65eae4cbf18e43d063b3f7d97e5953b07ea
violedu <violeduinternal@gmail.com>
2024-07-11 12:05:14 +0200

    incorporated Google Fonts using Next.js's Automatic Webfont Optimization feature

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,8 @@ export default class MyDocument extends Document {
               `,
             }}
           />
+
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
