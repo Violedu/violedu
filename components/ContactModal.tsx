6b86f9f25fda6ca39f2006611b02ccb01b01190e
violedu <violeduinternal@gmail.com>
2024-12-03 21:49:09 +0100

    added header text

--- components/ContactModal.tsx
+++ components/ContactModal.tsx
@@ -15,7 +15,9 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
         className={styles.modalContainer}
         onClick={(e) => e.stopPropagation()} // Prevent click propagation to the wrapper
       >
-        <div className={styles.modalHeader}></div>
+        <div className={styles.modalHeader}>
+          <span className={styles.modalHeaderText}>Send a message</span>
+        </div>
         {/* Modal content can go here */}
       </div>
     </div>
