796f5a93a9ef6d06b68897620932beaccfd585a2
violedu <violeduinternal@gmail.com>
2024-12-08 22:02:55 +0100

    fixed spacing

--- components/ContactModal.tsx
+++ components/ContactModal.tsx
@@ -47,46 +47,50 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
         <div className={styles.modalHeader}>
           <span className={styles.modalHeaderText}>Send a message</span>
         </div>
-        <div className={styles.content}>
-          <div className={styles.circleWrapper}>
-            <div className={`${styles.profileCircle} ${styles.firstCircle}`}>
-              <img src="/profile_kalina.png" alt="Profile" className={styles.profileImage} />
+        <div className={styles.mainContent}>
+          <div className={styles.content}>
+            <div className={styles.circleWrapper}>
+              <div className={`${styles.profileCircle} ${styles.firstCircle}`}>
+                <img src="/profile_kalina.png" alt="Profile" className={styles.profileImage} />
+              </div>
+              <div className={`${styles.profileCircle} ${styles.secondCircle}`}>
+                <img src="/profile_ivo.png" alt="Profile" className={styles.profileImage} />
+              </div>
             </div>
-            <div className={`${styles.profileCircle} ${styles.secondCircle}`}>
-              <img src="/profile_ivo.png" alt="Profile" className={styles.profileImage} />
+            <p className={styles.replyText}>We typically reply within a day</p>
+            <p className={styles.helperText}>We are here to help you reach your violin goals.</p>
+          </div>
+          <div className={styles.formButtonContainer}>
+            <div className={styles.form}>
+              <TextField
+                className={styles.textField}
+                label="Email"
+                variant="outlined"
+                fullWidth
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={errors.email}
+                helperText={errors.email ? "Please enter a valid email address" : ""}
+              />
+              <TextField
+                className={styles.textField}
+                label="Message"
+                variant="outlined"
+                multiline
+                rows={5}
+                fullWidth
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                error={errors.message}
+                helperText={errors.message ? "Message cannot be empty" : ""}
+              />
+            </div>
+            <div className={styles.buttonContainer}>
+              <button className={styles.sendButton} onClick={onSendMessage}>
+                <div className={styles.submitRequest}>Send a message</div>
+              </button>
             </div>
           </div>
-          <p className={styles.replyText}>We typically reply within a day</p>
-          <p className={styles.helperText}>We are here to help you reach your violin goals.</p>
-        </div>
-        <div className={styles.form}>
-          <TextField
-            className={styles.textField}
-            label="Email"
-            variant="outlined"
-            fullWidth
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            error={errors.email}
-            helperText={errors.email ? "Please enter a valid email address" : ""}
-          />
-          <TextField
-            className={styles.textField}
-            label="Message"
-            variant="outlined"
-            multiline
-            rows={5}
-            fullWidth
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            error={errors.message}
-            helperText={errors.message ? "Message cannot be empty" : ""}
-          />
-        </div>
-        <div className={styles.buttonContainer}>
-          <button className={styles.sendButton} onClick={onSendMessage}>
-            <div className={styles.submitRequest}>Send a message</div>
-          </button>
         </div>
       </div>
     </div>
