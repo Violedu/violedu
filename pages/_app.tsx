f1a2ecccd16da8efa7d5e10f4bb1e4c4854eb60d
violedu <violeduinternal@gmail.com>
2025-01-06 14:27:16 +0100

    added metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,54 @@ export default function MyApp(props: AppProps) {
       <DialogProvider>
         <Fragment>
           <Head>
-            <title>violedu</title>
+            {/* Title Tag */}
+            <title>Online Violin Lessons for Intermediate & Advanced Players | Violedu</title>
+            
+            {/* Meta Description */}
+            <meta
+              name="description"
+              content="Step up your next violin performance with personalized online violin lessons, expert feedback and progress tracking for intermediate to advanced violinists."
+            />
+            
+            {/* Keywords */}
+            <meta
+              name="keywords"
+              content="violin lessons, learn violin, online violin courses, violin tutorials, music education, violin practice, music lessons online"
+            />
+            
+            {/* Open Graph Tags */}
+            <meta property="og:title" content="Online Violin Lessons | Violedu" />
+            <meta
+              property="og:description"
+              content="Step up your next violin performance with personalized online violin lessons, expert feedback and progress tracking for intermediate to advanced violinists."
+            />
+            <meta property="og:image" content="https://www.violedu.com/logo_open_graph.png" />
+            <meta property="og:url" content="https://www.violedu.com" />
+            <meta property="og:type" content="website" />
+
+            {/* Structured Data (JSON-LD) */}
+            <script
+              type="application/ld+json"
+              dangerouslySetInnerHTML={{
+                __html: JSON.stringify({
+                  "@context": "http://schema.org",
+                  "@type": "Organization",
+                  "name": "Violedu",
+                  "foundingDate": "2023",
+                  "email": "contact@violedu.com",
+                  "url": "https://www.violedu.com",
+                  "logo": {
+                    "@type": "ImageObject",
+                    "url": "https://www.violedu.com/logo_structured_data.png",
+                    "name": "Violedu Logo",
+                    "width": "196",
+                    "height": "196"
+                  }
+                })
+              }}
+            />
+            
+            {/* Viewport */}
             <meta
               name="viewport"
               content="minimum-scale=1, initial-scale=1, width=device-width"
