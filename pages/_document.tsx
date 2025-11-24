5f8e1f7725b3bbbff615d1bda5d482986738bef5
Violedu <152742289+Violedu@users.noreply.github.com>
2025-11-24 09:13:56 +0100

    Update _document.tsx, fix GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,40 +1,12 @@
+// violedu/pages/_document.tsx
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-const gtag = `https://www.googletagmanager.com/gtag/js?id=G-S7PW6X8LQ1`;
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {/* 1. First set disable flag */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                // Block GA by default until consent
-                window['ga-disable-G-S7PW6X8LQ1'] = true;
-              `,
-            }}
-          />
-          
-          {/* 2. Then load GA script */}
-          <script async src={gtag} />
-          
-          {/* 3. Initialize dataLayer and check cookies */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                
-                // Check cookies AFTER GA script loads
-                if (document.cookie.includes('cookieAccepted=true')) {
-                  window['ga-disable-G-S7PW6X8LQ1'] = false;
-                  gtag('config', 'G-S7PW6X8LQ1');
-                }
-              `,
-            }}
-          />
+          {/* Only necessary links remain here, like fonts */}
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
             rel="stylesheet"
